a97b4dfeb6c4a859d0c97ca04b75f9b05e94b2f0
Vinicio <vinicioxavierlv@gmail.com>
2023-05-24 15:52:55 -0500

    se agrega la funcion de editar

--- app.js
+++ app.js
@@ -1,118 +1,155 @@
-// Variables globales
+// Obtener referencia al formulario y a la lista de actividades
+const formulario = document.getElementById('formulario');
+const listaActividades = document.getElementById('listaActividades');
 
-const formularioUI = document.querySelector('#formulario');
-const listaActividadesUI = document.getElementById('listaActividades');
-let arrayActividades = [];
-
-
-// Funciones
-
-const CrearItem = (actividad) => {
-
-  let item = {
-    actividad: actividad,
-    estado: false
-  }
-
-  arrayActividades.push(item);
-
-  return item;
-
-}
-
-
-
-const GuardarDB = () => {
-
-  localStorage.setItem('rutina', JSON.stringify(arrayActividades));
-
-  PintarDB();
-
-}
-
-const PintarDB = () => {
-
-  listaActividadesUI.innerHTML = '';
-
-  arrayActividades = JSON.parse(localStorage.getItem('rutina'));
+// Función para crear una nueva actividad
+const crearActividad = (actividad) => {
+  // Crear elemento div para la actividad
+  const divActividad = document.createElement('div');
+  divActividad.classList.add('alert', 'alert-danger');
+  divActividad.innerHTML = `
+    <i class="material-icons float-left mr-2">book</i>
+    <b>${actividad}</b> - Pendiente
+    <span class="float-right">
+      <i class="material-icons done">done</i>
+      <i class="material-icons edit">edit</i>
+      <i class="material-icons delete">delete</i>
+    </span>
+  `;
   
-  if(arrayActividades === null){
-    arrayActividades = [];
-  }else{
+  // Agregar la actividad al DOM
+  listaActividades.appendChild(divActividad);
+};
 
-    arrayActividades.forEach(element => {
+// Función para guardar la actividad en el almacenamiento local
+const guardarActividad = (actividad) => {
+  // Obtener las actividades guardadas previamente
+  const actividades = obtenerActividades();
 
-      if(element.estado){
-        listaActividadesUI.innerHTML += `<div class="alert alert-success" role="alert"><i class="material-icons float-left mr-2">book</i><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">edit</i><i class="material-icons">delete</i></span></div>`
-      }else{
-        listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"><i class="material-icons float-left mr-2">book</i><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">edit</i><i class="material-icons">delete</i></span></div>`
-      }
-    });
+  // Agregar la nueva actividad al arreglo
+  actividades.push(actividad);
 
+  // Guardar el arreglo actualizado en el almacenamiento local
+  localStorage.setItem('actividades', JSON.stringify(actividades));
+};
+
+// Función para obtener las actividades guardadas en el almacenamiento local
+const obtenerActividades = () => {
+  const actividadesString = localStorage.getItem('actividades');
+  
+  // Si no hay actividades guardadas, retornar un arreglo vacío
+  if (!actividadesString) {
+    return [];
   }
-}
+  
+  // Convertir el string JSON a un arreglo y retornarlo
+  return JSON.parse(actividadesString);
+};
 
-const EliminarDB = (actividad) => {
-  let indexArray;
-  arrayActividades.forEach((elemento, index) => {
-    
-    if(elemento.actividad === actividad){
-      indexArray = index;
-    }
-    
+// Función para cargar las actividades almacenadas en el inicio
+const cargarActividades = () => {
+  // Obtener las actividades guardadas
+  const actividades = obtenerActividades();
+
+  // Recorrer el arreglo de actividades y crear los elementos en el DOM
+  actividades.forEach(actividad => {
+    crearActividad(actividad);
   });
+};
 
-  arrayActividades.splice(indexArray,1);
-  GuardarDB();
+// Función para cambiar el estado de una actividad a "Hecha"
+const cambiarEstado = (elemento) => {
+  const actividadElemento = elemento.parentElement.parentElement;
+  actividadElemento.classList.remove('alert-danger');
+  actividadElemento.classList.add('alert-success');
+  actividadElemento.innerHTML = actividadElemento.innerHTML.replace('Pendiente', 'Hecha');
+};
 
-}
+// Función para editar una actividad
+// Función para editar una actividad
+const editarActividad = (elemento) => {
+  const actividadElemento = elemento.parentElement.parentElement;
+  const actividadTexto = actividadElemento.querySelector('b').textContent;
 
-const EditarDB = (actividad) => {
+  // Cargar los datos de la actividad en el formulario
+  const actividadInput = document.getElementById('actividad');
+  actividadInput.value = actividadTexto;
 
-  let indexArray = arrayActividades.findIndex((elemento)=>elemento.actividad === actividad);
+  // Actualizar la actividad al presionar el botón "Guardar"
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  arrayActividades[indexArray].estado = true;
+    const nuevaActividad = actividadInput.value;
 
-  GuardarDB();
+    if (nuevaActividad && nuevaActividad.trim() !== '') {
+      // Actualizar el texto de la actividad en el elemento del DOM
+      actividadElemento.querySelector('b').textContent = nuevaActividad;
 
-}
+      // Actualizar la actividad en el almacenamiento local
+      const actividades = obtenerActividades();
+      const index = actividades.indexOf(actividadTexto);
+      actividades[index] = nuevaActividad;
+      localStorage.setItem('actividades', JSON.stringify(actividades));
+
+      // Limpiar el formulario y eliminar el evento de submit
+      actividadInput.value = '';
+      formulario.removeEventListener('submit', onSubmit);
+    }
+  });
+};
 
 
-// EventListener
+// Función para eliminar una actividad
+const eliminarActividad = (elemento) => {
+  const actividadElemento = elemento.parentElement.parentElement;
+  const actividadTexto = actividadElemento.querySelector('b').textContent;
+  
+  actividadElemento.remove();
+  
+  // Obtener las actividades guardadas
+  const actividades = obtenerActividades();
+  
+  // Filtrar las actividades para eliminar la actividad correspondiente
+  const nuevasActividades = actividades.filter(actividad => actividad !== actividadTexto);
+  
+  // Guardar el arreglo actualizado en el almacenamiento local
+  localStorage.setItem('actividades', JSON.stringify(nuevasActividades));
+};
 
-formularioUI.addEventListener('submit', (e) => {
-
+// Evento para agregar una nueva actividad
+formulario.addEventListener('submit', (e) => {
   e.preventDefault();
-  let actividadUI = document.querySelector('#actividad').value;
 
-  if(actividadUI.value === ''){
-    alert('Todos los campos se deben llenar');
-    return;
+  const actividadInput = document.getElementById('actividad');
+  const nuevaActividad = actividadInput.value;
+
+  if (nuevaActividad && nuevaActividad.trim() !== '') {
+    crearActividad(nuevaActividad);
+    guardarActividad(nuevaActividad);
+    actividadInput.value = '';
   }
-
-  CrearItem(actividadUI);
-  GuardarDB();
-
-  formularioUI.reset();
-
 });
 
-document.addEventListener('DOMContentLoaded', PintarDB);
-
-listaActividadesUI.addEventListener('click', (e) => {
-
-  e.preventDefault();
-
-  if(e.target.innerHTML === 'done' || e.target.innerHTML === 'delete'){
-    let texto = e.path[2].childNodes[1].innerHTML;
-    if(e.target.innerHTML === 'delete'){
-      // Accción de eliminar
-      EliminarDB(texto);
-    }
-    if(e.target.innerHTML === 'done'){
-      // Accción de editar
-      EditarDB(texto);
-    }
+// Evento para cambiar el estado de una actividad al hacer clic en el icono "done"
+listaActividades.addEventListener('click', (e) => {
+  if (e.target.classList.contains('done')) {
+    cambiarEstado(e.target);
   }
-
 });
+
+// Evento para editar una actividad al hacer clic en el icono "edit"
+listaActividades.addEventListener('click', (e) => {
+  if (e.target.classList.contains('edit')) {
+    editarActividad(e.target);
+  }
+});
+
+// Evento para eliminar una actividad al hacer clic en el icono "delete"
+listaActividades.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    eliminarActividad(e.target);
+  }
+});
+
+// Cargar las actividades almacenadas en el inicio
+cargarActividades();
